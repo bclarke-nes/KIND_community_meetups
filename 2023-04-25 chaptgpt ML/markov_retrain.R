fc35e505061a1c6f05609805d1b90804a69f0475
Brendan Clarke <brendan.clarke2@nhs.scot>
2023-04-21 14:25:57 +0100

    tidied up script

--- 2023-04-25 chaptgpt ML/markov_retrain.R
+++ 2023-04-25 chaptgpt ML/markov_retrain.R
@@ -1,10 +1,14 @@
+library(pacman)
+p_load(tidyverse, gutenbergr, tidytext)
 
-book_learnin <- function(gut_n, n_out) {
+book_learnin <- function(gut_n, n_out, to_lower=TRUE, strip_punct=TRUE) {
 
   corpus <- gutenberg_download(gut_n) %>%
-  unnest_tokens(word, text) %>%
+  unnest_tokens(word, text, to_lower=to_lower, strip_punct=strip_punct) %>%
   select(!gutenberg_id) 
 
+  
+?unnest_tokens
 # find out which words occur and how often
 freq <- corpus %>%
   count(word, sort=T) %>%
@@ -44,12 +48,38 @@ n_words <- function(df, n) {
   glue::glue_collapse(out, " ")
 }
 
+build_sentence <- function(df) {
+  # approach shamelessly stolen from whoever chatGPT took it from
+  # find the first word
+  first <- df$freq %>%
+    filter(str_detect(word, "^[A-Z]")) %>%
+    pull(word, f)
+  
+  # start building a sentence around that first word
+  sentence <- unname(sample(first, 1, replace=T, prob=names(first)))
+  
+  while (!any(grep("[.!?]$", sentence))) {
+  # add next words based on sampling probability until end of sentence
+    next_word <- df$p_words %>% 
+      select_groups(which(df$group_keys %in% sentence[length(sentence)])) %>%
+      pull(next_word, p) %>%
+      sample(., 1, replace=T, prob=names(.))
+    sentence <- c(sentence, next_word)
+  }
+  out <- paste(unname(sentence), collapse=" ")
+  out <- str_replace_all(out, "\\s(?=[\\.,!?;])", "")
+  out
+}
 
-frankenstein <- book_learnin(84) # frankenstein
+frankenstein_2 <- book_learnin(84, to_lower=FALSE, strip_punct=FALSE) # frankenstein
 n_words(frankenstein, 50)
+build_sentence(frankenstein_2)
 
 moby_dick <- book_learnin(2701) # moby_dick
 n_words(moby_dick, 50)
+
+moby_dick_2 <- book_learnin(2701, to_lower=FALSE, strip_punct=FALSE)
+build_sentence(moby_dick_2)
 
 middlemarch <- book_learnin(145) # middlemarch
 n_words(middlemarch, 50)
@@ -57,5 +87,6 @@ n_words(middlemarch, 50)
 austen <- book_learnin(31100) # Austen
 n_words(austen, 50)
 
-big <- book_learnin(1:500)
-n_words(big, 50)
+# big <- book_learnin(1:500)
+# big <- read_rds("big.rds")
+# n_words(big, 50)
